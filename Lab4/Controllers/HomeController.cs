56ac50ac24897cf1813723ccdc6deb9faeed8b3c
aquinoc2 <aquinoc2@D017LAB-06>
2018-10-11 10:30:57 -0400

    Add project files.

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("Lab4");
+        }
+
+    }       
+    
+}
---
+++ Lab4/Controllers/Lab4Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime date1 = DateTime.Now;
+            return NewMethod();
+        }
+
+        private IActionResult NewMethod()
+        {
+            return View();
+        }
+    }
+}
